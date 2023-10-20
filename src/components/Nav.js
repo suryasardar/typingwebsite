8f1582a3a802716b86f3a9d28814761330dc65a3
suryasardar <suryaprakashchary616@gmail.com>
2023-10-20 12:36:32 +0530

    done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
   return (
     <>
       <div className="logo-container">
-        <img src="/logo2.jpg" alt="Logo" className="logo-image" />
+        <img src="/download.jpeg" alt="Logo" className="logo-image" />
         <span className="logo-text">TYPING</span>
         <NavLink to="/">
           <button className="text-btn">Text</button>
